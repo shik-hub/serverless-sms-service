4662939559bc8245db211b20eda5e7cf2cb8388b
shikhin arora <shikhin@hubhello.com>
2022-11-06 14:51:27 +0000

    Fixed api response, fixed stage variables

--- functions/test.js
+++ functions/test.js
@@ -1,17 +1,39 @@
-const handler = (event) => {
+const handler = async (event) => {
+  console.log("inside handler");
   try {
-    console.log(event);
-    return {
-      status: 200,
+    // console.log(event);
+    console.log("inside try");
+    return generateResponse(200, {
       message: "Its working",
-    };
+    });
   } catch (error) {
+    console.log("inside catch");
     console.error(error);
-    return {
-      status: 500,
+    return generateResponse(500, {
       message: "Some error occurred",
-    };
+    });
   }
+};
+
+const generateResponse = (statusCode, body) => {
+  console.log("generating response");
+  const isBase64Encoded = false;
+  const headers = {
+    "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+    "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+    "Content-Type": "text/plain",
+  };
+
+  const response = {
+    statusCode,
+    isBase64Encoded,
+    body: JSON.stringify(body),
+    headers,
+  };
+
+  console.log("response: ", response);
+
+  return response;
 };
 
 module.exports = {
