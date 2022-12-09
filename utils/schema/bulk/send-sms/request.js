4fb7900fb674df2f333f2fdde2c6c3880363f162
shikhin arora <shikhin@hubhello.com>
2022-12-09 17:35:34 +0000

    Updated schema of bulk lambda

--- utils/schema/bulk/send-sms/request.js
+++ utils/schema/bulk/send-sms/request.js
@@ -1,22 +1,43 @@
 const Ajv = require("ajv");
 const ajv = new Ajv();
 
+const userArray = {
+  type: "object",
+  properties: {
+    phoneNumber: { type: "string", pattern: "^\\+\\d{8,14}$" },
+    recipientName: { type: "string", minLength: 1 },
+    recipientId: { type: "integer", minimum: 0 },
+  },
+  required: ["phoneNumber", "recipientId", "recipientName"],
+};
+
 const requestSchema = {
   type: "object",
   properties: {
-    phoneNumbers: {
+    users: {
       type: "array",
-      items: {
-        type: "string",
-        pattern: "^\\+\\d{8,14}$",
-      },
+      items: userArray,
       minItems: 1,
       maxItems: 200,
     },
     message: { type: "string", minLength: 1 },
     type: { enum: ["Promotional", "Transactional"] },
+    category: { type: "string", minLength: 1 },
+    requestUserId: { type: "integer", minimum: 0 },
+    clientId: { type: "integer", minimum: 0 },
+    enterpriseId: { type: "integer", minimum: 0 },
+    groupId: { type: "integer", minimum: 0 },
   },
-  required: ["phoneNumbers", "message", "type"],
+  required: [
+    "users",
+    "message",
+    "type",
+    "category",
+    "requestUserId",
+    "clientId",
+    "enterpriseId",
+    "groupId",
+  ],
   additionalProperties: false,
 };
 
